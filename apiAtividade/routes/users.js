5569ce993f222a594a89d119bb0cbd4dd3eb44b1
pedrorivesa <114407947+pedrorivesa@users.noreply.github.com>
2022-11-22 11:41:11 -0300

    Criação do banco de dados

--- apiAtividade/routes/users.js
+++ apiAtividade/routes/users.js
@@ -2,8 +2,43 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [ 
+    {
+        nome: "Lucky",
+        cpf: 7777,
+        DataDeNascimento: 20/50/52,
+        Telefone: 302341,
+        eMail: "Lucky@email.com",
+        cidade: "Maringa",
+        estado: "Parana"  
+    
+    },
+
+    {
+        nome: "Joy",
+        cpf: 4545,
+        DataDeNascimento: 20/50/29,
+        Telefone: 7513,
+        eMail: "Joy@email.com",
+        cidade: "Maringa",
+        estado: "Parana"
+    }
+]
+
 router.get('/users', (req,res) =>{
-    res.send('Teste');
+    
+
+    res.send(users);
+});
+
+router.post('/', (req,res) =>{
+    
+
+    const user = req.body;
+
+    users.push(user);
+    
+    res.send(`Usuario com o nome ${user.nome} adicionado com sucesso` ); 
 });
 
 export default router;
